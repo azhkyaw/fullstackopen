d8ac39945c45f3cb6868d2d355d35f58d339e054
Aung Zwe Htet Kyaw <azhk.dev@gmail.com>
2024-08-06 15:56:32 +0800

    Add solutions for exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,8 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
 import { voteOf } from "../reducers/anecdoteReducer";
+import Filter from "./Filter";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) =>
+    filter === ""
+      ? anecdotes
+      : anecdotes.filter((a) =>
+          a.content.toLowerCase().includes(filter.toLowerCase())
+        )
+  );
   const dispatch = useDispatch();
 
   const vote = (id) => {
@@ -12,6 +19,7 @@ const AnecdoteList = () => {
 
   return (
     <>
+      <Filter />
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
